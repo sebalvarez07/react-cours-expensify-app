5d15529e71e8871e831b9e8a73d1bf9797466245
Sebastian Alvarez <>
2019-04-14 15:28:36 -0400

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,53 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, '/public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, '/public'),
-        historyApiFallback: true    // This tells the browser that routing will happen client side. So fetch index.html on every call to the site (localhost:8080 e.g)
+module.exports = (env) => {
+
+    const isProduction = env === 'production';
+    const cssExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, '/public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: cssExtract.extract({
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true,
+
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true,
+                                }
+                            }
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins: [
+            cssExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, '/public'),
+            historyApiFallback: true    // This tells the browser that routing will happen client side. So fetch index.html on every call to the site (localhost:8080 e.g)
+        }
     }
-}
+};
