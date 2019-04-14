2a53f6613114396a9aa918d6a078989136444b4c
Sebastian Alvarez <>
2019-04-14 19:45:50 -0400

    Fromatted amount and date on ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 // We can destructure as we're passing the object expense's contents as a set of props (using spread operator)
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
@@ -8,8 +10,8 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => (
             <Link to={`/edit/${id}`} >Description: {description}</Link>
         </h3>
         
-        <p>Amount: {amount}</p>
-        <p>createdAt {createdAt}</p>
+        <p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
+        <p>Date created: {moment(createdAt).format('Do, MMMM, YYYY')}</p>
         <br></br> 
     </div>
 );
